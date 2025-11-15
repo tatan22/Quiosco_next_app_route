ba3c21fb61617293315b6680b728b3fbd12ea0d1
Jhonatan Cardona D <109439934+tatan22@users.noreply.github.com>
2025-11-15 17:02:35 -0500

    Solucion Errorres

--- app/admin/products/search/page.tsx
+++ app/admin/products/search/page.tsx
@@ -1,16 +1,20 @@
-import ProductsSearchForm from "@/components/products/ProductsSearchForm";
+import ProductsPagination from "@/components/products/ProductsPagination";
 import ProductTable from "@/components/products/ProductsTable";
 import Heading from "@/components/ui/Heading";
 import { prisma } from "@/src/lib/prisma";
+import { redirect } from "next/navigation";
+import Link from "next/link";
+import ProductsSearchForm from "@/components/products/ProductsSearchForm";
 
-async function searchProducts(searchTerm: string) {
+async function productsCount() {
+	return await prisma.product.count();
+}
+
+async function getProducts(page: number, pageSize: number) {
+	const skip = (page - 1) * pageSize;
 	const products = await prisma.product.findMany({
-		where: {
-			name: {
-				contains: searchTerm, //contains es un filtro de búsqueda (existen: startsWith, endsWith, contains, not,...)
-				mode: "insensitive", // Ignorar mayúsculas
-			},
-		},
+		take: pageSize,
+		skip,
 		include: {
 			category: true,
 		},
@@ -19,28 +23,48 @@ async function searchProducts(searchTerm: string) {
 	return products;
 }
 
-export default async function page({
+export type ProductsWithCategory = Awaited<ReturnType<typeof getProducts>>;
+
+export default async function ProductsPage({
 	searchParams,
 }: {
-	searchParams: { search: string };
+	searchParams?: Promise<{ page?: string }>;
 }) {
-	const search = searchParams.search;
+	const resolvedSearchParams = await searchParams;
+	const rawPage = resolvedSearchParams?.page;
+	const page = rawPage ? Number(rawPage) : 1;
+	const pageSize = 10;
 
-	const products = await searchProducts(search);
+	if (page < 1) redirect("/admin/products");
+
+	const productsData = getProducts(page, pageSize);
+	const totalProductsData = productsCount();
+	const [products, totalProducts] = await Promise.all([
+		productsData,
+		totalProductsData,
+	]);
+	const totalPages = Math.ceil(totalProducts / pageSize);
+
+	if (page > totalPages && totalPages > 0) {
+		redirect("/admin/products");
+	}
 
 	return (
 		<>
-			<Heading>Resultados de la búsqueda: {searchParams.search}</Heading>
+			<Heading>Administrar Productos</Heading>
 
-			<div className="flex flex-col lg:flex-row lg:justify-end gap-5 justify-between">
+			<div className="flex flex-col lg:flex-row gap-5 justify-between">
+				<Link
+					href={`/admin/products/new`}
+					className="bg-amber-400 w-full lg:w-auto text-xl px-10 py-3 text-center font-bold cursor-pointer"
+				>
+					Crear Producto
+				</Link>
 				<ProductsSearchForm />
 			</div>
 
-      {products.length ? (
-        <ProductTable products={products} />
-      ): <p className="text-center text-lg mt-10">No se encontraron productos</p> }
-
-			
+			<ProductTable products={products} />
+			<ProductsPagination page={page} totalPages={totalPages} />
 		</>
 	);
 }
